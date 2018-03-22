3d7f17b2286e2d225c2e8758f238883d464efd40
liu haha <web0376@126.com>
2018-03-22 11:18:49 +0800

    人脸识别代码上传

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # baidu_Face
 百度人脸识别SDK for php
+百度在线文档：[https://cloud.baidu.com/doc/FACE/Face-PHP-SDK.html](https://cloud.baidu.com/doc/FACE/Face-PHP-SDK.html)  
+
---
+++ demo.php
@@ -0,0 +1,25 @@
+<?php  
+require_once __DIR__ . '/vendor/autoload.php';  
+ 
+use BaiduFace\Api\AipFace;  
+ 
+const APP_ID = '10912480';
+const API_KEY = 'ZQKX0raHY54437xVG8guv5y9';
+const SECRET_KEY = 'HqPkze2SpHILOePMHZeeaE9NdAQG9l20';
+
+$client = new AipFace(APP_ID, API_KEY, SECRET_KEY);
+
+$image = file_get_contents('mayun.jpg');
+
+// 调用人脸检测
+$client->detect($image);
+
+// 如果有可选参数
+$options = array();
+$options["max_face_num"] = 1;
+$options["face_fields"] = "age,gender";
+
+// 带参数调用人脸检测
+$res = $client->detect($image, $options);
+//echo '<pre>';
+var_dump($res);   
